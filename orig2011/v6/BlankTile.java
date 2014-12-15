7585be3016a4258623244a87202bb8e9c486a9bb
criticalstone <kevinhoog@gmail.com>
2014-12-15 17:07:12 +0100

    Adds package for part 6 of the lab

---
+++ orig2011/v6/BlankTile.java
@@ -0,0 +1,12 @@
+package orig2011.v6;
+
+import java.awt.*;
+
+public class BlankTile implements GameTile {
+
+    @Override
+    public void draw(Graphics g, int x, int y, Dimension d) {
+        // The default GameTile is transparent,
+        // therefore no drawing is performed.
+    }
+}
