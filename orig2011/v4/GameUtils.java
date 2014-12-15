fe5bbc5469c64f94cc1ffab6ebd4a214f61a5d1c
criticalstone <kevinhoog@gmail.com>
2014-12-15 16:25:26 +0100

    Creates a new package for part four of the lab

---
+++ orig2011/v4/GameUtils.java
@@ -0,0 +1,12 @@
+package orig2011.v4;
+
+import java.awt.*;
+
+public final class GameUtils {
+    /**
+     * Returns the size of the gameboard.
+     */
+    public static Dimension getGameboardSize() {
+        return Constants.getGameSize();
+    }
+}
